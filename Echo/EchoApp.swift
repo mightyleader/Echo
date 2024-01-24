5b7381a9aa6171649e3e72dcbeab4438ab77911d
Rob Stearn <robstearn@me.com>
2024-01-24 15:05:39 +0000

    Initial Commit

---
+++ Echo/EchoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoApp.swift
+//  Echo
+//
+//  Created by Rob Stearn on 24/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct EchoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
